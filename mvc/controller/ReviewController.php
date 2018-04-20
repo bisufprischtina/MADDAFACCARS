cf285a36604b045fe4eb1c9ab0dd920ba812a779
Bieri Dominik <bbierd@bbc-be.local>
2018-04-20 11:17:35 +0200

    sepplipremhetverreckigsproblem#5Klassischläbägsi xD

--- mvc/controller/ReviewController.php
+++ mvc/controller/ReviewController.php
@@ -33,7 +33,11 @@ class ReviewController
             $marke = $_POST['marke'];
             $modell = $_POST['modell'];
             $review = $_POST['review'];
-            $image = $_POST['image'];
+            $image = $_FILES['image'];
+            echo "<pre>";
+            var_dump($_FILES);
+            $_FILES['image']
+//            move_uploaded_file(filToUpload)
 
             $reviewRepository = new ReviewRepository();
             $reviewRepository->create($marke, $modell, $review, $image);
@@ -43,6 +47,62 @@ class ReviewController
         header('Location: /review');
 
     }
+
+    public function upimage(){
+        //image extensions allowed
+        $allowedExts = array("gif", "jpeg", "jpg", "png");
+        $temp = explode(".", $_FILES["file"]["name"]);
+        $extension = end($temp);
+        if ((($_FILES["file"]["type"] == "image/gif")
+                || ($_FILES["file"]["type"] == "image/jpeg")
+                || ($_FILES["file"]["type"] == "image/jpg")
+                || ($_FILES["file"]["type"] == "image/pjpeg")
+                || ($_FILES["file"]["type"] == "image/x-png")
+                || ($_FILES["file"]["type"] == "image/png"))
+            && ($_FILES["file"]["size"] < 20000)
+            && in_array($extension, $allowedExts))
+        {
+            if ($_FILES["file"]["error"] > 0)
+            {
+                echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+            }
+            else
+            {
+
+                //Successfully uploaded
+                echo "Your file " . $_FILES["file"]["name"] . " successfully uploaded!!<br>";
+                echo "Details :";
+                echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+                echo "Type: " . $_FILES["file"]["type"] . "<br>";
+                echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+
+                //Display Image
+                echo "<img src=uploaded/" . $_FILES["file"]["name"] . ">";
+
+                //Uploaded image folder
+                if (file_exists("uploaded/" . $_FILES["file"]["name"]))
+                {
+                    echo $_FILES["file"]["name"] . " already exists. ";
+                }
+                else
+                {
+                    move_uploaded_file($_FILES["file"]["tmp_name"],
+                        "uploaded/" . $_FILES["file"]["name"]);
+
+                }
+            }
+        }
+        else
+        {
+            //error message on the extension that are not allowed
+            echo "Invalid file";
+            $filename = preg_replace('/[^A-Z0-9]/','',$_FILES["file"]["name"]) . $extension;
+            $logo = uploaded/$filename;
+
+            //insert into database
+            $strSQL = "INSERT INTO $table(name,image) VALUES('$name_file','$logo')";
+            mysql_query($strSQL) or die(mysql_error());
+        }
+    }
 }
 
-?>
--- mvc/view/review_create.php
+++ mvc/view/review_create.php
@@ -9,7 +9,7 @@ echo $form->text()->label('Model')->name('modell');
 echo $form->text()->label('Your review (max. 1000)')->name('review');
 
 echo "<p>Insert picture of your car</p>";
-echo $form->image()->label('')->name('sendreview');
+echo $form->image()->label('')->name('image');
 
 // echo $form->password()->label('Password')->name('password');
 echo $form->submit()->label('Submit review')->name('sendreview');
